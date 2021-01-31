ae7a44bd9cf3a6fd1403d0d88cc07031ca87bbd6
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-01-31 21:27:45 +0100

    Load data and compute scales

--- src/Visualisation.svelte
+++ src/Visualisation.svelte
@@ -1,14 +1,67 @@
 <script>
+  import {
+    csv,
+    ascending,
+    scalePoint,
+    scaleLinear,
+    extent,
+  } from 'd3';
+
   import Svg from './components/Svg.svelte';
 
   export let size = 0;
+
+  const dataPath = 'data/nature2019.csv';
+  const starRadius = { min: 0.5, max: 6 };
+  const margin = starRadius.max;
+
+  $: innerRadius = 0.25 * (size / 2);
+  $: outerRadius = (size - 2 * margin) / 2;
+
+  let data = [];
+
+  csv(dataPath, (d) => {
+    const year = +d.Year;
+    const volume = +d.Volume;
+    const issue = +d.Issue;
+
+    return {
+      authors: d.Authors,
+      title: d.Title,
+      journal: d['Source title'],
+      year,
+      volume,
+      issue,
+      location: [year, volume, issue].join('/'),
+      page: { start: +d['Page start'], end: +d['Page end'] },
+      citedBy: +d['Cited by'],
+      doi: d['DOI'],
+    };
+  }).then((loaded) => {
+    data = loaded.sort(
+      (a, b) =>
+        ascending(a.volume, b.volume) ||
+        ascending(a.issue, b.issue) ||
+        ascending(a.page, b.page))
+  });
+
+  const angleScale = scalePoint()
+    .domain(data.map((d) => d.location))
+    .range([(2 * Math.PI) / data.length, 2 * Math.PI]);
+
+  const skyScale = scaleLinear()
+    .domain(extent(data, d => d.citedBy))
+    .range([innerRadius, outerRadius]);
 </script>
 
 <div
   class="visualisation-wrapper"
   style="width: {size}px; height: {size}px"
 >
-  <Svg {size} />
+  <Svg {size}>
+    <circle fill="none" stroke="red" r={innerRadius} />
+    <circle fill="none" stroke="red" r={outerRadius} />
+  </Svg>
 </div>
 
 <style>
