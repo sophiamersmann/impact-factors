8932b7fe02ed0087b823658a6bdb2c4bb5b997c4
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-02-03 17:08:38 +0100

    Tween quantile lines

--- src/components/QuantileLine.svelte
+++ src/components/QuantileLine.svelte
@@ -2,17 +2,27 @@
   import { interpolateLab } from 'd3-interpolate';
 
   import { tweened } from 'svelte/motion';
+  import { quadOut } from 'svelte/transition';
 
   import {
     axisLine as lineColor,
     selectedAxisLine as selectedLineColor
   } from '../inputs/colors';
 
-  import { shortDuration } from '../stores/configurations';
+  import {
+    duration,
+    shortDuration,
+    longDuration
+  } from '../stores/configurations';
 
   export let radius = 0;
   export let selected = false;
 
+  const tweenedRadius = tweened(radius, {
+    delay: $duration,
+    duration: $longDuration,
+    easing: quadOut,
+  });
   const color = tweened(lineColor, {
     duration: $shortDuration,
     interpolate: interpolateLab,
@@ -21,6 +31,7 @@
     duration: $shortDuration,
   })
 
+  $: tweenedRadius.set(radius);
   $: color.set(selected ? selectedLineColor : lineColor);
   $: opacity.set(selected ? 1 : 0.2);
 </script>
@@ -29,7 +40,7 @@
   class="axis-line"
   cx="0"
   cy="0"
-  r={radius}
+  r={$tweenedRadius}
   stroke={$color}
   stroke-opacity={$opacity}
 />
