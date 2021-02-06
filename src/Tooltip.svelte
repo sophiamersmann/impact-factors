b5731a9e5860c8463522a32d095874c7dbdd18c2
Sophia Mersmann <sophia.mersmann1@gmail.com>
2021-02-06 22:18:50 +0100

    Update tooltip style

--- src/Tooltip.svelte
+++ src/Tooltip.svelte
@@ -15,17 +15,21 @@
 {#if $selectedStar}
   <div
     class="tooltip"
-    style="width: {size}px; height: {size}px"
+    style="width: {size}px"
     in:fly={{ x: size, duration: $duration, easing: quadOut }}
     out:fly={{ x: size, duration: $duration, easing: quadIn }}
   >
     <div class="content">
-      <span aria-hidden='true'></span>
-      <b>{$selectedStar.title}</b> {authors}.
-      <span class="journal">{$selectedStar.journal}</span> {$selectedStar.volume},
-      {$selectedStar.page.start}-{$selectedStar.page.end}
-      ({$selectedStar.year})
-      <div class="cited-by">Cited <b>{$selectedStar.citedBy}</b> times</div>
+      <b>{$selectedStar.title}</b>
+      <div>
+        {authors}.
+        <span class="journal">{$selectedStar.journal}</span> {$selectedStar.volume},
+        {$selectedStar.page.start}-{$selectedStar.page.end}
+        ({$selectedStar.year})
+      </div>
+      <div class="cited-by">
+        Cited <b>{$selectedStar.citedBy}</b> times
+      </div>
       </div>
     </div>
 {/if}
@@ -36,16 +40,12 @@
     right: var(--spacing);
     top: var(--spacing);
 
-    display: grid;
-    place-items: center;
-    
-    padding: 20px;
+    padding: calc(var(--spacing) / 2);
     color: white;
     background-color: var(--background-color);
     font-size: 0.9rem;
-    border-radius: 50%;
-    text-align: center;
     border: 2px solid white;
+    border-radius: 10px;
     box-shadow: 0 16px 48px rgba(255, 255, 255, 0.08);
   }
 
@@ -59,26 +59,9 @@
     border-radius: 5px;
     font-size: 0.8rem;
     background-color: white;
-    width: max-content;
     color: var(--background-color);
     padding: 2px 4px;
-    margin: 6px auto;
+    text-align: center;
+    margin-top: calc(var(--spacing) / 2);
   }
-
-  [aria-hidden='true']:after,
-  [aria-hidden='true']:before {
-    float: left;
-    width: 50%; height: 100%;
-    --bite: radial-gradient(farthest-side
-                            at var(--pos-x, 100%), 
-                            transparent 100%, red);
-    -webkit-shape-outside: var(--bite);
-            shape-outside: var(--bite);
-    content: ''
-  }
-
-  [aria-hidden='true']:after {
-		float: right;
-		--pos-x: 0
-	}
 </style>
